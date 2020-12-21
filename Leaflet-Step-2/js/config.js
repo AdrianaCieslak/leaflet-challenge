08c78fd87b0bb808a2bcfae067a7a01ff04c66ac
adriana cieslak <adrianciac@yahoo.com>
2020-12-21 14:33:07 -0500

    completed

---
+++ Leaflet-Step-2/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYWRyaTAwIiwiYSI6ImNraTg2ZTBnazAycXgycXBkOXRpejUybzcifQ.ol3quF1NZixHIh0EqX1ouA";
---
+++ Leaflet-Step-2/js/logic.js
@@ -0,0 +1,137 @@
+
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+var url2 = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_plates.json";
+
+function chooseColor(mag) {
+    switch (true) {
+    case mag < 1:
+      return "#40ff00";
+    case mag < 2:
+      return "#bfff00";
+    case mag < 3:
+      return "#ffff00";
+    case mag < 4:
+      return "#ffbf00";
+    case mag < 5:
+      return "#ff8000";
+    default:
+      return "#ff0000";
+    }
+  }
+  
+function magSize(magnitude) {
+    return magnitude * 5;
+}
+
+// Perform a GET request to the query URL
+d3.json(url, function(data) {
+  
+  createFeatures(data.features);
+  console.log(data)
+});
+
+var plates = new L.LayerGroup();
+
+d3.json(url2, function(ge))
+function createFeatures(earthquakeData) {
+    
+  function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+          "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " + feature.properties.mag + "</p>");
+      }
+    
+    var earthquakes = L.geoJSON(earthquakeData, {
+        pointToLayer: function(feature, coords) {
+            return new L.circleMarker(coords, {
+                radius: magSize(feature.properties.mag),
+                color: "black",
+                fillColor: chooseColor(feature.properties.mag),
+                fillOpacity: 0.8,
+                weight: 1
+            });
+
+
+        },
+    onEachFeature: onEachFeature   
+  });
+
+
+
+// Sending our earthquakes layer to the createMap function
+createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v9",
+    accessToken: API_KEY
+  });
+
+  var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v9",
+    accessToken: API_KEY
+  });
+
+  var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v9",
+    accessToken: API_KEY
+  });  
+  // Define baseMap
+  var baseMaps = {
+    "Satellite": satellite,
+    "Grayscale": grayscale,
+    "Outdoors": outdoors
+    };
+
+  // Create overlay
+  var overlayMaps = {
+    "Earthquakes": earthquakes,
+    "Tectonic Plates": plates
+
+  };
+
+  // Create our map
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -122.42
+    ],
+    zoom: 4,
+    layers: [satellite, earthquakes]
+  });
+
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  
+
+//Legend
+
+   var legend = L.control({
+     position: 'bottomleft'});
+
+   legend.onAdd = function () {
+   
+       var div = L.DomUtil.create('div', 'info legend')
+       
+       div.innerHTML = "<h3> Magnitude Legend </h3><table><tr><th>< 1</th><td>Harlequin</td></tr><tr><th>< 2</th><td>Lime</td></tr><tr><th><3</th><td>Yellow</td></tr><tr><th><4</th><td>Orange</td></tr><tr><th><5</th><td>Dark Orange</td></tr><tr><th>>5</th><td>Red</td></tr></table>";
+
+       return div;
+   };
+
+legend.addTo(myMap);
+}
